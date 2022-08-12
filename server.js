1b1e3b0041d8152a8c5a7b13d35237fb207ba1cb
MasoudMoeini <Masoud.Moeini@Studium.uni-hamburg.de>
2022-08-12 10:45:22 +0200

    update

--- server.js
+++ server.js
@@ -11,17 +11,15 @@ const mongoAuth = mongoUser && mongoPass ? `${mongoUser}:${mongoPass}@` : '';
 const mongoHost= process.env.RESTNODE_DB_HOST || 'localhost';
 const mongoPort = process.env.MONGO_PORT || 27017;
 const mongoDb = process.env.MONGO_DB || 'my_database';
-const connString = process.env.MONGODB_CONNSTRING || `mongodb://${mongoAuth}${mongoHost}:${mongoPort}/${mongoDb}`;
+const connString = process.env.MONGODB_CONNSTRING || `mongodb://${mongoAuth}${mongoHost}:${mongoPort}/${mongoDb}?authSource=admin`
 
 //Set up default mongoose connection
 mongoose.connect(connString, {useNewUrlParser: true, useUnifiedTopology: true});
-//Get the default connection
-var db = mongoose.connection;
-
 //mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
-//const db = mongoose.connection
-//db.on('error', (error) => console.error(error))
-//db.once('open', () => console.log('Connected to Database'))
+//Get the default connection
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
 //Set up API transactions 
 app.use(express.json())
